434c3d12c6b1c7425ffb668c56c03031977132b8
yaGeey <krabadaj@gmail.com>
2025-03-23 18:33:52 +0200

    make images unoptimized because of exceeding vercel's free plan of 1000 images

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['assets.ppy.sh', 'i.scdn.co', 'i.ytimg.com', 'i.imgur.com', 'lh3.googleusercontent.com'],
+    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   experimental: {
     reactCompiler: true,
-  }
+  },
 };
 
 export default nextConfig;
