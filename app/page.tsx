7ba0c5917868e25a082212c697683f7fb7df4f62
yaGeey <krabadaj@gmail.com>
2025-07-23 21:56:10 +0300

    repo info integration

--- app/page.tsx
+++ app/page.tsx
@@ -4,53 +4,69 @@ import { Button } from '@/components/buttons/Buttons'
 import BgImage from '@/components/BgImage'
 import Image from 'next/image'
 import { useEffect } from 'react'
-// import { useState } from "react";
-// import { twMerge as tw } from "tailwind-merge";
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+const GH_REPO = 'https://api.github.com/repos/yaGeey/osu-find-songs'
 
 export default function LandingPage() {
-   // const [src, setSrc] = useState<string>('/bg.svg')
-   // const [isVisible, setIsVisible] = useState<boolean>(false)
-   // function handleMouseEnter(img: string) {
-   //    setTimeout(() => {
-   //       setSrc(img)
-   //       setIsVisible(true)
-   //    }, 1000)
-   // }
-   // function handleMouseLeave() {
-   //    setIsVisible(false)
-   //    setTimeout(() => {
-   //       setSrc('/bg.svg')
-   //    }, 700)
-   // }
    useEffect(() => {
       if (localStorage.getItem('songs_context')) {
          localStorage.removeItem('songs_context')
       }
    }, [])
+
+   const { data: lastUpdated } = useQuery({
+      queryKey: ['lastUpdated'],
+      queryFn: async () => {
+         const { data } = await axios(`${GH_REPO}/commits?per_page=1`)
+         return {
+            date: new Date(data[0].commit.author.date).toLocaleString('en-US', {
+               dateStyle: 'short',
+               timeStyle: 'short',
+            }),
+            url: data[0].html_url,
+         }
+      },
+   })
+   const { data: stargazeCount } = useQuery({
+      queryKey: ['stargazeCount'],
+      queryFn: async () => {
+         const { data } = await axios(GH_REPO)
+         return data.stargazers_count
+      },
+   })
    return (
       <div className="flex flex-col justify-center items-center h-screen">
          <BgImage />
-         {/* <BgImage image={src} className={tw("duration-700 transition-all",
-            isVisible ? 'opacity-100' : 'opacity-0'
-         )} /> */}
          <div className="bg-dialog relative flex flex-col justify-center items-center bg-main-lighter rounded-2xl p-10 shadow-lg border-4 border-main-border">
             <Image src="/icon.png" width={75} height={75} alt="logo" />
             <h1 className="text-3xl  mt-3">Welcome to osu! find songs</h1>
             <h2 className="text-2xl ">Choose one of the options</h2>
             <div className="flex gap-4 mt-10 w-full">
                <Link href="/from-osu/select">
-                  {/* onMouseEnter={() => handleMouseEnter('/from-osu.png')} onMouseLeave={handleMouseLeave} */}
                   <Button className="text-black bg-gradient-to-l from-[#1DB954] to-[#FF66AA] font-medium">
                      Beatmaps to Spotify
                   </Button>
                </Link>
                <Link href="/from-spotify/select">
-                  {/* onMouseEnter={() => handleMouseEnter('/from-spotify.png')} onMouseLeave={handleMouseLeave} */}
                   <Button className="text-black bg-gradient-to-r from-[#1DB954] to-[#FF66AA] font-medium">
                      Spotify playlist to beatmaps
                   </Button>
                </Link>
             </div>
+            {lastUpdated && stargazeCount && (
+               <div className="absolute bottom-0 w-full flex px-2 justify-center text-xs text-main-border/80">
+                  <a href={GH_REPO} target="_blank">
+                     GitHub
+                     <FontAwesomeIcon icon={faStar} className="text-[10px] mx-0.5" />
+                     {stargazeCount} · Last updated: {lastUpdated.date}
+                     <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="text-[10px] ml-1" />
+                  </a>
+               </div>
+            )}
          </div>
       </div>
    )
