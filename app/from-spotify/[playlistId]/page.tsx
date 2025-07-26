b248a21f97a9a567b08d8499919f1573146b524a
yaGeey <krabadaj@gmail.com>
2025-07-26 22:35:01 +0300

    fix styling min block height

--- app/from-spotify/[playlistId]/page.tsx
+++ app/from-spotify/[playlistId]/page.tsx
@@ -202,7 +202,7 @@ export default function PLaylistPage() {
          </header>
 
          <main className="flex justify-center items-center min-h-[calc(100vh-4rem)] mt-[56px]">
-            <div className=" min-h-[calc(100vh-3.5rem)] [@media(min-width:980px)]:w-4/5 w-full  max-w-[1900px]">
+            <div className=" min-h-[calc(100vh-3.5rem)] bg-darker [@media(min-width:980px)]:w-4/5 w-full  max-w-[1900px]">
                <Filters
                   foundString={Array.isArray(maps) && maps.length ? maps.length + '/' + tracks.length : ''}
                   onChange={(val, searchTypeRes, mode) => {
