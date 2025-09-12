3f88314f872da903c1c03313859f91843cb07396
yaGeey <krabadaj@gmail.com>
2025-09-13 01:21:01 +0300

    pb f

--- upload-sourcemaps.js
+++ upload-sourcemaps.js
@@ -4,8 +4,7 @@ import { execSync } from "child_process"
 const projectRoot = process.cwd()
 const nextBuildDir = path.join(projectRoot, ".next")
 
-const commitSHA = process.env.NEXT_PUBLIC_COMMIT_SHA
-const appVersion = `${process.env.HIGHLIGHT_APP_NAME}-${commitSHA ? commitSHA : "local"}`
+const appVersion = `${process.env.HIGHLIGHT_APP_NAME}-${process.env.VERCEL_GIT_COMMIT_SHA}`
 
 const cmd = `npx @highlight-run/sourcemap-uploader upload --apiKey "${process.env.HIGHLIGHT_API_KEY}" --appVersion "${appVersion}" --path "${nextBuildDir}" --basePath /var/task/.next`
 execSync(cmd, { stdio: "inherit" })
