6dece71e369e1fb593b59bbd9e6a04c880d003d4
yaGeey <krabadaj@gmail.com>
2025-12-23 10:30:24 +0200

    even more seo opt

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -7,8 +7,15 @@ const title = 'osufindsongs – find osu! songs & convert 🎵'
 const url = 'https://osu.yageey.me'
 
 export const metadataObj: Metadata = {
+   metadataBase: new URL(url),
    title,
    description,
+   applicationName: 'osufindsongs',
+   authors: [{ name: 'yageey', url: 'https://yageey.me' }],
+   creator: 'yageey',
+   alternates: {
+      canonical: '/',
+   },
    // prettier-ignore
    keywords: [
       // Core terms
@@ -32,14 +39,16 @@ export const metadataObj: Metadata = {
    openGraph: {
       title,
       description,
-      images: [{ url: '/preview.png' }],
+      images: [{ url: '/preview.png', alt: 'osufindsongs preview' }],
       locale: 'en_US',
+      type: 'website',
+      url,
    },
    twitter: {
       card: 'summary_large_image',
       title,
       description,
-      images: [`${url}/icon.png`],
+      images: [`/preview.png`],
    },
    robots: {
       follow: true,
@@ -62,7 +71,12 @@ export function JsonLd() {
       description,
       applicationCategory: 'MultimediaApplication',
       screenshot: `${url}/preview.png`,
-      operatingSystem: "Windows, macOS, Linux, Web",
+      operatingSystem: 'Windows, macOS, Linux, Web',
+      sameAs: ['https://github.com/yaGeey/osu-find-songs'],
+      audience: {
+         '@type': 'Audience',
+         audienceType: 'osu! players, Rhythm game enthusiasts',
+      },
       featureList: [
          'Convert Spotify playlists to osu! beatmaps',
          'Scan osu! beatmaps and create Spotify playlists',
@@ -75,11 +89,60 @@ export function JsonLd() {
          price: '0',
          priceCurrency: 'USD',
       },
-      subjectOf: {
-         '@type': 'VideoObject',
-         name: 'ThunderBirdo featured osufindsongs in his video',
-         contentUrl: 'https://www.youtube.com/watch?v=0uZ4RehxDO4',
+      creator: {
+         '@type': 'Person',
+         name: 'yageey',
+         url: ['https://yageey.me', 'https://github.com/yaGeey'],
       },
+      subjectOf: [
+         {
+            '@type': 'VideoObject',
+            name: "The top 15 most UNDERRATED Osu! addons (It get's insane!)",
+            description:
+               'Discover 15 game-changing Osu! addons, from map preview tools to custom hit circle generators. This video showcases essential community-made resources, including a replay analyzer and settings sharer.',
+            thumbnailUrl: 'https://i.ytimg.com/vi/0uZ4RehxDO4/maxresdefault.jpg',
+            uploadDate: '2025-07-11T17:13:53-07:00',
+            contentUrl: 'https://www.youtube.com/watch?v=0uZ4RehxDO4&t=300s',
+            embedUrl: 'https://www.youtube.com/embed/0uZ4RehxDO4',
+            author: {
+               '@type': 'Person',
+               name: 'ThunderBirdo',
+               url: 'https://www.youtube.com/@ThunderBirdo',
+            },
+         },
+         // 2. Reddit Пост (Авторський анонс)
+         {
+            '@type': 'SocialMediaPosting',
+            headline: 'I made a web app that allows users to find songs from their osu! folder on Spotify',
+            url: 'https://www.reddit.com/r/osugame/comments/1jgntcz/i_made_a_web_app_that_allows_users_to_find_songs/',
+            commentCount: 37,
+            publisher: {
+               '@type': 'Organization',
+               name: 'Reddit',
+            },
+         },
+         // 3. Reddit Пост (Update/Fix)
+         {
+            '@type': 'SocialMediaPosting',
+            headline: 'I fixed a web app that allows users to find songs',
+            url: 'https://www.reddit.com/r/osugame/comments/1m5ruu2/i_fixed_a_web_app_that_allows_users_to_find_songs/',
+            commentCount: 16,
+            publisher: {
+               '@type': 'Organization',
+               name: 'Reddit',
+            },
+         },
+         // 4. Каталог osuck.net
+         {
+            '@type': 'WebPage',
+            name: 'osu! find songs by ssable - osu! tools and resources',
+            url: 'https://tools.osuck.net/tool/67df097dafb780368707339a',
+            publisher: {
+               '@type': 'Organization',
+               name: 'osuck.net',
+            },
+         },
+      ],
    }
    return (
       <script
