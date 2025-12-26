c77f8c8fb652008dd4fd735b6a7a181bdbbd1e82
yaGeey <krabadaj@gmail.com>
2025-12-26 16:05:01 +0200

    m: removed modals goofy border animation

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -25,12 +25,12 @@ export default function Modal({
       <div className={`fixed top-0 left-0 w-screen h-screen bg-black/30 z-10000 text-center`}>
          <div
             className={tw(
-               'overflow-hidden animate-border [--color-animated-body:var(--color-main-light)] text-black text-balance max-w-[540px] border-4 rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3.5 transition-all shadow-2xl flex flex-col',
-               status === 'error' && '[--color-animated-border:var(--color-error)]',
-               status === 'success' && '[--color-animated-border:var(--color-success)]',
-               status === 'warning' && '[--color-animated-border:var(--color-warning)]',
-               status === 'info' && '[--color-animated-border:var(--color-main-subtle)]',
-               status === 'loading' && '[--color-animated-border:var(--color-main-subtle)]',
+               'overflow-hidden bg-triangles text-black _animate-border _[--color-animated-body:var(--color-main-light)] text-balance max-w-[540px] border-4 rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3.5 transition-all shadow-2xl flex flex-col',
+               status === 'error' && '[--color-animated-border:var(--color-error)] border-error',
+               status === 'success' && '[--color-animated-border:var(--color-success)] border-success',
+               status === 'warning' && '[--color-animated-border:var(--color-warning)] border-warning',
+               status === 'info' && '[--color-animated-border:var(--color-main-subtle)] border-main-border',
+               status === 'loading' && '[--color-animated-border:var(--color-main-subtle)] border-main-border',
             )}
          >
             <div className="flex-1 p-3 flex flex-col gap-2 justify-center items-center font-medium">{children}</div>
