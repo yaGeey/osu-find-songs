2b0cb730414c4e3257d23dc391b4d8ed14be4d8f
yaGeey <krabadaj@gmail.com>
2026-02-14 02:47:28 +0200

    style(f-s): input's ux ui

--- app/from-osu/select/page.tsx
+++ app/from-osu/select/page.tsx
@@ -2,17 +2,20 @@
 import { Song } from '@/types/types'
 import { useSongContext } from '@/contexts/SongContext'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
-import { ToastContainer, toast } from 'react-toastify'
-import { getServerToken } from '@/lib/Spotify'
+import { ToastContainer } from 'react-toastify'
 import Footer from '@/components/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCopy, faUpload } from '@fortawesome/free-solid-svg-icons'
+import { twJoin } from 'tailwind-merge'
+import { getServerToken } from '@/lib/Spotify'
+// import useGetLazySpotifyTokens from '@/hooks/useGetLazySpotifyTokens'
+import { sendUnknownError } from '@/lib/client-axios'
 
-// prettier-ignore
 export default function SelectPage() {
    const { setSongs } = useSongContext()
+   // useGetLazySpotifyTokens()
    const router = useRouter()
 
    useEffect(() => {
@@ -31,13 +34,13 @@ export default function SelectPage() {
 
    async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
       await getServerToken()
-   
+
       const files = e.target.files
       if (!files || files.length === 0) throw new Error('No files selected')
-      
+
       const songs: Song[] = []
       const fileList = Array.from(files)
-      
+
       // grouping files by folders
       const folders = new Map<string, File[]>()
       for (const file of fileList) {
@@ -57,12 +60,12 @@ export default function SelectPage() {
          const id = songParts.length > 0 && !isNaN(parseInt(songParts[0])) ? songParts.shift() : null
          if (!id) continue
 
-         const osuFile = files.find(f => f.name.endsWith('.osu'))
+         const osuFile = files.find((f) => f.name.endsWith('.osu'))
          if (!osuFile) continue
 
          // getting file content -> bg filename
          const content = await readFileAsText(osuFile)
-         const lines = content.split('\n') 
+         const lines = content.split('\n')
          let bgFileName: null | string = null
 
          lines.forEach((line, i) => {
@@ -73,9 +76,9 @@ export default function SelectPage() {
             }
          })
          if (!bgFileName) continue
-         
+
          // searching bg file
-         const imageFile = files.find(f => f.name === bgFileName)
+         const imageFile = files.find((f) => f.name === bgFileName)
          if (!imageFile) continue
          const image = URL.createObjectURL(imageFile)
 
@@ -93,43 +96,55 @@ export default function SelectPage() {
       router.push('/from-osu')
    }
 
+   const [message, setMessage] = useState('Click to select folder')
+   const [state, setState] = useState<'idle' | 'success' | 'error'>('idle')
+
    return (
       <div className="flex flex-col justify-center items-center min-h-screen text-white">
          <div className="flex flex-col justify-center items-center flex-1 text-nowrap">
             <h1 className="text-4xl tracking-tight font-semibold mb-3">Select your osu! beatmaps folder</h1>
             <h3 className="text-lg text-white/60">This may take some time</h3>
-            <div className="text-xl flex items-center gap-2">
-               <h2
-                  className="cursor-pointer hover:underline active:text-main-white"
-                  onClick={(e) => {
-                     navigator.clipboard.writeText(e.currentTarget.innerText)
-                     toast.success('Copied to clipboard!')
-                  }}
-               >
-                  %LocalAppData%\osu!\Songs
-               </h2>
+            <div
+               className="text-xl flex items-center gap-2 cursor-pointer hover:underline active:text-main-white"
+               onClick={async (e) => {
+                  navigator.clipboard.writeText(e.currentTarget.innerText)
+                  setMessage('Path copied to clipboard!')
+                  await new Promise((resolve) => setTimeout(resolve, 5000))
+                  setMessage('Click to select folder')
+               }}
+            >
+               <h2>%LocalAppData%\osu!\Songs</h2>
                <FontAwesomeIcon icon={faCopy} />
             </div>
-            <div className="border-2 border-dashed w-full border-main rounded-lg mt-7 p-8 text-center relative hover:brightness-125 transition-all">
-               <div className="text-main pointer-none">
+            <div
+               className={twJoin(
+                  'border-2 border-dashed w-full rounded-lg mt-7 p-8 text-center relative hover:brightness-125 transition-all',
+                  state === 'idle' && 'border-main text-main',
+                  state === 'success' && 'border-success text-success',
+                  state === 'error' && 'border-error text-error',
+               )}
+            >
+               <div>
                   <FontAwesomeIcon icon={faUpload} className="text-5xl mb-3" />
-                  <div className="text-lg font-medium">Click to select folder</div>
+                  <div className="text-lg font-medium">{message}</div>
                </div>
                <input
                   type="file"
                   {...({ directory: '', webkitdirectory: '' } as any)}
-                  onChange={(e) => {
-                     toast.promise(handleFileChange(e), {
-                        pending: 'Loading beatmaps...',
-                        error: {
-                           render({ data }) {
-                              console.error(data)
-                              return 'Please select a valid osu! beatmaps directory'
-                           },
-                           autoClose: false,
-                           hideProgressBar: true,
-                        },
-                     })
+                  onChange={async (e) => {
+                     setState('idle')
+                     setMessage('Loading...')
+                     handleFileChange(e)
+                        .then(() => {
+                           setState('success')
+                           setMessage('Redirecting...')
+                        })
+                        .catch((e) => {
+                           setState('error')
+                           setMessage('Failed. Check console for more details')
+                           console.error(e)
+                           sendUnknownError(e, 'FILE_SELECT')
+                        })
                   }}
                   className="absolute opacity-0 top-0 left-0 w-full h-full"
                />
